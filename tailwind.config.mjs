cc8c52a1eb2df7b1850501a6146e05993d14bb9f
alvaromateosrobledillo <alvaromar988@gmail.com>
2024-05-16 18:18:43 +0200

    improve template things

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				lightBackground: '#ffffff',
+				darkBackground: '#1a202c',
+				lightText: '#000000',
+				darkText: '#ffffff',
+				lightBorder: '#e2e8f0',
+				darkBorder: '#4a5568',
+			},
+		},
 	},
 	plugins: [],
 }
