9b074f8bdbcb94c3b3a5fc048bdac93a60f9eac2
Eduardo  Tebar Ibañez <eduardotebaribanez@MacBook-Pro-de-Eduardo.local>
2024-06-17 10:35:07 +0200

    ultimos cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				lightBackground: '#ffffff',
-				darkBackground: '#1a202c',
-				lightText: '#000000',
-				darkText: '#ffffff',
-				lightBorder: '#e2e8f0',
-				darkBorder: '#4a5568',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        lightBackground: "#ffffff",
+        darkBackground: "#1a202c",
+        lightText: "#000000",
+        darkText: "#ffffff",
+        lightBorder: "#e2e8f0",
+        darkBorder: "#4a5568",
+        accent: "#d97706" /* Amber-500 */,
+      },
+    },
+  },
+  plugins: [],
+};
